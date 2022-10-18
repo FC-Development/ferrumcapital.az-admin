66c2a09fbd3fbdeaff06b4e82cfcb371c1d4bd6a
shariflikenan <kenan.sherifli@ferrumcapital.az>
2022-10-18 12:18:49 +0400

    .

--- app/Http/Controllers/MainWeb/MkSliderController.php
+++ app/Http/Controllers/MainWeb/MkSliderController.php
@@ -11,17 +11,15 @@ use App\Models\MkSlider;
 
 class MkSliderController extends Controller
 {
-    public MkSlider $mk_slider;
-    public MkSliderAction $mk_action;
 
     public function mksliderPage(Request $request)
     {
         return view('dashboard.main-web.customer-slider');
     }
-    public function getData(MkSliderAction $mk_action)
+    public function getData()
     {
-        $this->mk_action= $mk_action;
-        $mk_slider_data = $this->mk_action->getData();
+        $mk_action = new MkSliderAction();
+        $mk_slider_data = mk_action->getData();
         return $mk_slider_data;
     }
 }
