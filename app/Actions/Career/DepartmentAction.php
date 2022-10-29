bd73e5e63d1bf4235cad701a770e148e9fbf8ef3
shariflikenan <kenan.sherifli@ferrumcapital.az>
2022-10-29 13:42:29 +0400

    adding delete functionality to DepartmentAction.php

--- app/Actions/Career/DepartmentAction.php
+++ app/Actions/Career/DepartmentAction.php
@@ -52,9 +52,7 @@ class DepartmentAction extends \App\Abstracts\AdminMethods
         try {
             $uniq_id = $request->input("uniq_id");
             $vacancy = VacancyModel::where("department_id",$uniq_id)->get();
-            return response()->json($vacancy);
-            if($this->department->vacancies()) {
-                return response()->json($this->department->vacancies(),404);
+            if(sizeof($vacancy) > 0) {
                 $data = $this->department->where("uniq_id",$uniq_id)->delete();
                 return response()->json($data);
             }
