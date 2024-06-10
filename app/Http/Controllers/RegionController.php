cb6138fdac704eba66cb63f580eb10bdd2d66370
eynullabeyli <eynullabeyliy@gmail.com>
2024-06-10 18:11:26 +0400

    up

--- app/Http/Controllers/RegionController.php
+++ app/Http/Controllers/RegionController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Region;
 
 class RegionController extends Controller
 {
-    //
+    /**
+     * Display a listing of the regions for a given city_id.
+     *
+     * @param  int  $city_id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getRegionsByCityId($city_id)
+    {
+        $regions = Region::where('city_id', $city_id)->select('region_id', 'region_name')->get();
+        return response()->json($regions);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainWeb\MkSliderController;
 use App\Http\Controllers\CityController;
+use App\Http\Controllers\RegionController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,5 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::get("/get/FerrumCapital/MusteriKabineti/slider",[MkSliderController::class,'getData']);
 
-//get city names
+//get city and region names
 Route::get('cities/names', [CityController::class, 'getCityNames']);
+Route::get('regions/{city_id}', [RegionController::class, 'getRegionsByCityId']);
