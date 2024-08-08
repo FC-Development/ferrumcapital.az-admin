6ff955e2ccf7cc0c2d8d6751b5191e9b1fd1353d
eynullabeyli <eynullabeyliy@gmail.com>
2024-08-08 16:37:48 +0400

    updme

--- public/dashboard_/assets/js/campaigns.js
+++ public/dashboard_/assets/js/campaigns.js
@@ -52,6 +52,10 @@ let CampaignListTable = new gridjs.Grid({
             <button class="btn btn-sm  delete-Campaigns mr-1" data-uniq-id="${card.uniq_id}">
             <svg xmlns="http://www.w3.org/2000/svg" height="22" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#DA1B24" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+            </svg></button>
+            <button class="btn btn-sm  edit-Campaigns mr-1" data-uniq-id="${card.uniq_id}">
+            <svg xmlns="http://www.w3.org/2000/svg" height="22" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#000" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
             </svg></button></div>`)
         ]),
     }
@@ -86,6 +90,13 @@ if(top.location.pathname === '/dashboard/campaigns') {
                 console.log(data);
             }
         })
+    });
+    $(document).on("click", ".edit-Campaigns", function(e) {
+        //$("#loading").show()
+        var id = $(this).attr("data-uniq-id");
+        console.log(id);
+        $("#edit_campaign_title_input").val("ddd")
+        $("#CampaignEditModal").modal("show");
     })
     $("#NewCampaignForm").submit((e) => {
         e.preventDefault();
--- resources/views/dashboard/main-web/campaigns.blade.php
+++ resources/views/dashboard/main-web/campaigns.blade.php
@@ -89,5 +89,79 @@
             </div>
         </div>
     </div>
+    <div class="modal fade hide pr-0" id="CampaignEditModal" tabindex="-1" aria-modal="false" role="dialog">
+        <div class="modal-dialog" role="document" style="min-width: 700px;">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Seçilmiş kampaniya məlumatlarını dəyiş</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <form class="modal-body" id="NewCampaignForm" enctype=multipart/form-data>
+                    @csrf
+                    <div class="row">
+                        <div class="col-md-12 mb-3">
+                            <div class="partnerSelectForCampaign">
+                                <label for="campaign_partner_input" class="f-sm form-label font-weight-bold text-muted text-uppercase">Partnyor</label>
+                                <select class="form-control" id="edit_campaign_partner_input" name="edit_campaign_partner_input" require>
+                                    <option value="Seçim edilməyib" selected>Seçim edilməyib</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <label
+                            for="campaign_title_input"
+                            class="f-sm form-label font-weight-bold text-muted text-uppercase">Kampaniya başlıqı</label>
+                            <input type="text"
+                            id="edit_campaign_title_input"
+                            class="form-control border border-light"
+                            name="edit_campaign_title_input"
+                            inlength="2" required>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <span class="f-sm form-label font-weight-bold text-muted text-uppercase">Şəkil</span>
+                            <div class="custom-file mt-8">
+                                <input type="file" class="custom-file-input" name="edit_image_campaign_input" accept="image/png, image/jpeg" required>
+                                <label for="image" class="custom-file-label">Seçim edin</label>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <span class="f-sm form-label font-weight-bold text-muted text-uppercase">Mobil Şəkil</span>
+                            <div class="custom-file mt-8">
+                                <input type="file" class="custom-file-input" name="edit_image_mobile_campaign_input" accept="image/png, image/jpeg" required>
+                                <label for="image" class="custom-file-label">Seçim edin</label>
+                            </div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <span class="f-sm form-label font-weight-bold text-muted text-uppercase">Açıqlama</span>
+                            <br>
+                            <textarea id="edit_CampaignModalEditor_input"></textarea>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <label for="campaign_lastdate_input">Son tarix</label>
+                            <input type="date" class="form-control" name="edit_campaign_lastdate_input" id="edit_campaign_lastdate_input">
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <div class="input-group mb-4">
+                                <div class="input-group-prepend">
+                                    <label class="input-group-text" for="campaign_status_input">Status</label>
+                                </div>
+                                <select class="custom-select" id="edit_campaign_status_input" name="edit_campaign_status_input">
+                                    <option value="0">Aktiv</option>
+                                    <option value="1">Deaktiv</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-4" style="float: right;">
+                        <div class="col-lg-12">
+                            <button type="submit" class="btn btn-primary">Tamamla</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
   </div>
 @endsection
